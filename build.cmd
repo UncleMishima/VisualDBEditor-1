60807712ade59d41520d5d3e89ed4792cba7ebfc
VEL <VEL4EG@yandex.ru>
2017-11-02 07:11:04 +0300

    Updated build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-qmake.exe .\VisualDBEditor.pro -spec win32-g++ "CONFIG+=release" "DESTDIR=.\Build" 
+qmake.exe .\VisualDBEditor.pro -spec win32-g++
 mingw32-make.exe qmake_all
 mingw32-make -f Makefile.Release
